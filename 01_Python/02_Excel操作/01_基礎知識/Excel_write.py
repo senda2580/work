0be1e1d758d25fc1790ba38270362f56a9ed3467
sento <ucan-lab@example.com>
2023-09-23 02:14:33 +0900

    修正

--- 01_Python/02_Excel操作/01_基礎知識/Excel_write.py
+++ 01_Python/02_Excel操作/01_基礎知識/Excel_write.py
@@ -3,3 +3,11 @@ import openpyxl
 
 #ワークブックを読み込む
 wb = openpyxl.load_workbook("Excel\Write_Only.xlsx")
+
+#アクティブなワークシートを取得する
+ws = wb.active
+
+#セルA2に値を入力する
+ws["A2"].value = "TEST"
+
+wb.save("Excel\Write_Only.xlsx")
